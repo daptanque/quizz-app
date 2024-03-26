14f3cc288bb6a6a2e199c9fe1cb857fd6dbe5d42
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-03-26 16:58:34 +0000

    Use singleton for the atributes result/totalQuestions
    Plus reset it and set viewModelProvider + dataBinding

--- app/src/main/java/com/example/quizzapp/view/MainActivity.kt
+++ app/src/main/java/com/example/quizzapp/view/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.quizzapp.view
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.quizzapp.R
+import com.example.quizzapp.databinding.ActivityMainBinding
+import com.example.quizzapp.model.QuestionsListItem
+import com.example.quizzapp.viewmodel.QuizViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var quizViewModel: QuizViewModel
+    lateinit var questionsList: List<QuestionsListItem>
+
+
+    companion object{
+        var result = 0
+        var totalQuestions = 0
+
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        //Reset the score
+        result = 0
+        totalQuestions = 0
+
+        //Getting the response
+        quizViewModel = ViewModelProvider(this)
+            .get(QuizViewModel::class.java)
+
+
     }
 }
