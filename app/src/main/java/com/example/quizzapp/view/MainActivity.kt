224183c9387eb357c1a0f34dfe2ad52683209a44
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-03-26 17:45:10 +0000

    Logic to pass to the next question and assert if it is correct the option selected

--- app/src/main/java/com/example/quizzapp/view/MainActivity.kt
+++ app/src/main/java/com/example/quizzapp/view/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.quizzapp.view
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -56,6 +60,55 @@ class MainActivity : AppCompatActivity() {
                         radio4.text = questionsList!![0].option4
 
                     }
+                }
+            })
+        }
+
+        //Next Button
+        var i = 1
+        binding.apply {
+            btnNext.setOnClickListener(View.OnClickListener {
+                val selectedOption = radiogroup?.checkedRadioButtonId
+
+                if(selectedOption != -1){
+                    val radbutton = findViewById<View>(selectedOption!!) as RadioButton
+
+                    questionsList.let{
+                        if(i< it.size!!){
+                            totalQuestions = it.size
+                            if(radbutton.text.toString().equals(it[i-1].correct_option)){
+                                result++
+                                txtResult?.text="Correct Answer: $result"
+                            }
+
+                            //Next Question
+                            txtQuestion.text = "Question Number ${i+1}: " + questionsList[i].question
+                            radio1.text = it[i].option1
+                            radio2.text = it[i].option2
+                            radio3.text = it[i].option3
+                            radio4.text = it[i].option4
+
+                            //check Last question
+                            if(i == it.size!!.minus(1)){
+                                btnNext.text = "FINISH"
+                            }
+
+                            radiogroup?.clearCheck()
+                            i++
+                        }else{
+                            if(radbutton.text.toString().equals(it[i-1].correct_option)){
+                                result++
+                                txtResult?.text = "CorrectAnswer: $result"
+                            }else{
+
+                            }
+                            val intent = Intent(this@MainActivity, ResultActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+                }else{
+                    Toast.makeText(this@MainActivity,"Select an option", Toast.LENGTH_LONG).show()
                 }
             })
         }
