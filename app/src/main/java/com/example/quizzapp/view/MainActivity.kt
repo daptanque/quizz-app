cdcc1542f921c7f68e2926f089efb5cb944125b9
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-03-26 17:06:28 +0000

    Display first element

--- app/src/main/java/com/example/quizzapp/view/MainActivity.kt
+++ app/src/main/java/com/example/quizzapp/view/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.quizzapp.view
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.quizzapp.R
 import com.example.quizzapp.databinding.ActivityMainBinding
 import com.example.quizzapp.model.QuestionsListItem
 import com.example.quizzapp.viewmodel.QuizViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,6 +41,24 @@ class MainActivity : AppCompatActivity() {
         quizViewModel = ViewModelProvider(this)
             .get(QuizViewModel::class.java)
 
+        //Display first question - need to put this because despite running on MainThread you have the repo that is running on IO - getQuestions is suspend fun
+        GlobalScope.launch(Dispatchers.Main){
+            quizViewModel.getQuestionsFromLiveData().observe(this@MainActivity, Observer {
+                if(it.size>0){
+                    questionsList = it
+                    Log.i("TAGY", "The first questions: ${questionsList[0]}")
+
+                    binding.apply {
+                        txtQuestion.text = questionsList!![0].question
+                        radio1.text = questionsList!![0].option1
+                        radio2.text = questionsList!![0].option2
+                        radio3.text = questionsList!![0].option3
+                        radio4.text = questionsList!![0].option4
+
+                    }
+                }
+            })
+        }
 
     }
 }
