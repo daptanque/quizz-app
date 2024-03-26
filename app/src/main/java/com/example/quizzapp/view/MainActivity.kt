c0d8efbe74d843957317f353dbe5a5c5be434d90
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-03-26 16:39:25 +0000

    Setup Layout from Main Activity

--- app/src/main/java/com/example/quizzapp/view/MainActivity.kt
+++ app/src/main/java/com/example/quizzapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.quizzapp
+package com.example.quizzapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quizzapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
